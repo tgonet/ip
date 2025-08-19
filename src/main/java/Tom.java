421dc153d181ad0242dfc4358c3fb8b7ae39d3bb
tgonet <tanmingzhe.99@gmail.com>
2025-08-19 20:00:44 +0800

    Added List function
    - Updated the code to add a record to the list
    - Updated to show the list of records

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -1,30 +1,55 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Tom {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> ls = new ArrayList<String>();
 
         String logo = " _____ ___  __  __ \n"
-             + "|_   _/ _ \\|  \\/  |\n"
-             + "  | || | | | |\\/| |\n"
-             + "  | || |_| | |  | |\n"
-             + "  |_| \\___/|_|  |_|\n";
+                + "|_   _/ _ \\|  \\/  |\n"
+                + "  | || | | | |\\/| |\n"
+                + "  | || |_| | |  | |\n"
+                + "  |_| \\___/|_|  |_|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What do u need me to do?");
         System.out.println("____________________________________________________________");
 
-        String name;
+        String input;
         while (true) {
-            name = sc.nextLine();
-            if (name.equals("bye")) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                int count = 1;
+                if (ls.size() > 0) {
+                    System.out.println("____________________________________________________________");
+                    for (String i : ls) {
+                        if (i == null) {
+                            break;
+                        }
+                        System.out.println(count + ". " + i);
+                        count++;
+                    }
+                    System.out.println("____________________________________________________________");
+                }
+                else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("You have not added anything to the list!");
+                    System.out.println("____________________________________________________________");
+                }
+            } else {
+                if (ls.size() < 100) {
+                    ls.add(input);
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + input);
+                    System.out.println("____________________________________________________________");
+                }
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(name);
-            System.out.println("____________________________________________________________");
+
         }
     }
 }
