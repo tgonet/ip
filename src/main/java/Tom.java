5b31a4f2e39edc513ded0b32dc15cfa40b2f2378
tgonet <tanmingzhe.99@gmail.com>
2025-08-19 19:06:05 +0800

    Completed Level 0
    - Changed all "Duke" occurence to "Tom
    - Updated the welcome and exit message

---
+++ src/main/java/Tom.java
@@ -0,0 +1,15 @@
+public class Tom {
+    public static void main(String[] args) {
+        String logo = " _____ ___  __  __ \n"
+             + "|_   _/ _ \\|  \\/  |\n"
+             + "  | || | | | |\\/| |\n"
+             + "  | || |_| | |  | |\n"
+             + "  |_| \\___/|_|  |_|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("What do u need me to do?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        
+    }
+}
