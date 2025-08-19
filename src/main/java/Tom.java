ead53e28768adaebc1b5cd41adfa939790589677
tgonet <tanmingzhe.99@gmail.com>
2025-08-19 19:23:43 +0800

    Improved the current version to match Level 1
    - Chatbot now echos user input
    - Chatbot exits when bye is entered

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class Tom {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " _____ ___  __  __ \n"
              + "|_   _/ _ \\|  \\/  |\n"
              + "  | || | | | |\\/| |\n"
              + "  | || |_| | |  | |\n"
              + "  |_| \\___/|_|  |_|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
         System.out.println("What do u need me to do?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        
+
+        String name;
+        while (true) {
+            name = sc.nextLine();
+            if (name.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(name);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
