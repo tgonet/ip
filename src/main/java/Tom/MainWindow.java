323cb4f11454cb94dc8fd443d461c394c742998a
tgonet <tanmingzhe.99@gmail.com>
2025-09-18 13:33:43 +0800

    Update UI design
    Change the background with an image
    
    Update the chat bubble design

--- src/main/java/Tom/MainWindow.java
+++ src/main/java/Tom/MainWindow.java
@@ -6,11 +6,15 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
 import tom.customui.DialogBox;
 import tom.exception.TomException;
 import tom.ui.UI;
-
 
 /**
  * Controller for the main GUI.
@@ -29,12 +33,21 @@ public class MainWindow extends AnchorPane {
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/123.jpg"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/456.jpg"));
+    private BackgroundImage backgroundImage = new BackgroundImage(
+            new Image("/images/background.jpg"),
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundPosition.CENTER,
+            new BackgroundSize(
+                    100, 100, true, true, true, true) // adjust scaling
+    );
 
     private UI ui;
 
     @FXML
     public void initialize() {
         ui = new UI();
+        dialogContainer.setBackground(new Background(backgroundImage));
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
         dialogContainer.getChildren().addAll(
                 DialogBox.getDukeDialog(ui.printWelcome(), dukeImage));
@@ -61,10 +74,9 @@ public class MainWindow extends AnchorPane {
                     DialogBox.getDukeDialog(response, dukeImage));
         } catch (TomException e) {
             dialogContainer.getChildren().addAll(
-                 DialogBox.getUserDialog(input, userImage),
+                    DialogBox.getUserDialog(input, userImage),
                     DialogBox.getDukeDialog(e.getMessage(), dukeImage));
-        }
-        finally {
+        } finally {
             userInput.clear();
         }
     }
